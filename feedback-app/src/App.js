38a5d4b04bc265e23ba6f815aadf0e4ff51b6697
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-08-17 12:09:31 +0530

    routing using react-router-dom v5

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import feedbackData from './data/feedbacks';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const [feedbacks, setFeedbacks] = useState(feedbackData);
@@ -23,14 +25,23 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header text={'FEEDBACK UI'} />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedbacks={feedbacks} />
-        <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+        <Route exact path="/">
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedbacks={feedbacks} />
+          <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+        </Route>
+        {/* Method 1: of rendering route 
+        <Route exact path="/about">
+          This is some test HTML, we can render a component as well
+        </Route> 
+        */}
+        {/* Method 2: of rendering route */}
+        <Route exact path="/about" component={AboutPage} />
       </div>
-    </>
+    </Router>
   );
 }
 
