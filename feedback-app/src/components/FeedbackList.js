69f14c834159f2b42f0c8a459ad92df930a680b4
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-08-17 10:35:36 +0530

    added fade in fade out animation

--- feedback-app/src/components/FeedbackList.js
+++ feedback-app/src/components/FeedbackList.js
@@ -1,18 +1,37 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function FeedbackList({ feedbacks, handleDelete }) {
   // Conditional Rendering
   if (!feedbacks || feedbacks.length <= 0) {
     return <p>No Feedbacks yet.</p>;
   }
+
+  // WITH ANIMATION
   return (
     <div className="feedback-list">
-      {feedbacks.map(feedback => {
-        return <FeedbackItem key={feedback.id} item={feedback} handleDelete={handleDelete} />;
-      })}
+      <AnimatePresence>
+        {feedbacks.map(feedback => {
+          return (
+            <motion.div key={feedback.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              <FeedbackItem key={feedback.id} item={feedback} handleDelete={handleDelete} />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
+
+  // WITHOUT ANIMATION
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedbacks.map(feedback => {
+  //       return <FeedbackItem key={feedback.id} item={feedback} handleDelete={handleDelete} />;
+  //     })}
+  //   </div>
+  // );
 }
 
 // Adding the type checks of Array of which exact key and its type should be
