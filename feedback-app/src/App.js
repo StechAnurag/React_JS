646b17e2a6c3cf6bc106be77464aed0fb5fe0f09
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-08-17 20:29:19 +0530

    cleaning up and preparing scaffolding for context implementations

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
@@ -8,8 +7,6 @@ import FeedbackForm from './components/FeedbackForm';
 import feedbackData from './data/feedbacks';
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
-// import Card from './components/shared/Card';
-// import Post from './components/Post';
 
 function App() {
   const [feedbacks, setFeedbacks] = useState(feedbackData);
@@ -24,7 +21,6 @@ function App() {
   const addFeedback = newFeedback => {
     const newFeedbackId = new Date().getTime();
     newFeedback.id = newFeedbackId;
-    // state is immutable, so don't change it directly make a shallow copy of it
     setFeedbacks([newFeedback, ...feedbacks]);
   };
 
@@ -45,21 +41,7 @@ function App() {
             }
           ></Route>
           <Route exact path="/about" element={<AboutPage />} />
-          {/* <Route exact path="/post/:id/:name" element={<Post />} /> */}
-          {/* <Route path="/posts/*" element={<Post />} /> */}
         </Routes>
-
-        {/*
-        ----------- JUST A DEMO OF NavLinks ----------
-        <Card>
-          <NavLink to="/" activeClassName="active">
-            Home
-          </NavLink>{' '}
-          <NavLink to="/about" activeClassName="active">
-            About
-          </NavLink>
-        </Card> 
-        */}
       </div>
       <AboutIconLink />
     </Router>
