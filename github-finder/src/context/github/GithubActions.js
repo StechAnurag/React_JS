80fc1565942bb5c3be60e19c9d93889799ccab83
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-08-27 16:21:44 +0530

    axios instance and promise.all for mulitple requests in parallel

--- github-finder/src/context/github/GithubActions.js
+++ github-finder/src/context/github/GithubActions.js
@@ -1,60 +1,29 @@
+import axios from 'axios';
+
 const GITHUB_URL = process.env.REACT_APP_API_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`
+  }
+});
 
 export const searchUsers = async text => {
   try {
     const params = new URLSearchParams({
       q: text
     });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`
-      }
-    });
-
-    const { items: data } = await response.json();
-
-    return data;
+    const response = await github.get(`/search/users?${params}`);
+    return response.data.items;
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getUser = async login => {
-  try {
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`
-      }
-    });
-
-    if (response.status === 404) return (window.location = '/notfound');
-
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const getUserRepos = async login => {
-  try {
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10
-    });
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`
-      }
-    });
-    const data = await response.json();
-
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+// Get user and repos
+export const getUserAndRepos = async login => {
+  const [user, repos] = await Promise.all([github.get(`/users/${login}`), github.get(`/users/${login}/repos`)]);
+  return { user: user.data, repos: repos.data };
 };
