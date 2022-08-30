57db821972660d158d2bb199a4c7e12e6a644011
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-08-30 06:44:24 +0530

    register user to firebase auth and save user to firestore

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Explore from "./pages/Explore";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<Profile />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
--- house-marketplace/src/components/Navbar.js
+++ house-marketplace/src/components/Navbar.js
@@ -1,29 +1,52 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+// import { useNavigate, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
 import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
 
 function Navbar() {
-  const navigate = useNavigate();
-  const location = useLocation();
+  // const navigate = useNavigate();
+  // const location = useLocation();
 
-  const pathMatchRoute = route => route === location.pathname;
+  // const pathMatchRoute = route => route === location.pathname;
 
   return (
     <footer className="navbar">
       <nav className="navbarNav">
         <ul className="navbarListItems">
-          <li className="navbarListItem" onClick={() => navigate('/')}>
-            <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
-            <p className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p>
+          {/* <li className="navbarListItem" onClick={() => navigate('/')}> */}
+          {/* <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" /> */}
+          {/* <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" /> */}
+          {/* <p className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p> */}
+          <li className="navbarListItem">
+            <NavLink to="/">
+              {({ isActive }) => (
+                <>
+                  <ExploreIcon fill={isActive ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
+                  <p className={isActive ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p>
+                </>
+              )}
+            </NavLink>
           </li>
-          <li className="navbarListItem" onClick={() => navigate('/offers')}>
-            <OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
-            <p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offers</p>
+          <li className="navbarListItem">
+            <NavLink to="/offers">
+              {({ isActive }) => (
+                <>
+                  <p className={isActive ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offers</p>
+                  <OfferIcon fill={isActive ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
+                </>
+              )}
+            </NavLink>
           </li>
-          <li className="navbarListItem" onClick={() => navigate('/profile')}>
-            <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
-            <p className={pathMatchRoute('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Profile</p>
+          <li className="navbarListItem">
+            <NavLink to="/profile">
+              {({ isActive }) => (
+                <>
+                  <PersonOutlineIcon fill={isActive ? '#2c2c2c' : '#8f8f8f'} width="36px" height="36px" />
+                  <p className={isActive ? 'navbarListItemNameActive' : 'navbarListItemName'}>Profile</p>
+                </>
+              )}
+            </NavLink>
           </li>
         </ul>
       </nav>
