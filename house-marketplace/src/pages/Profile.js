1465c9f78d2e780583251c952c6cebee6201b479
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-11-25 07:36:47 +0530

    Profile page listings

--- house-marketplace/src/pages/Profile.js
+++ house-marketplace/src/pages/Profile.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
-import { updateDoc, doc } from 'firebase/firestore';
+import { updateDoc, doc, collection, getDocs, query, where, orderBy, deleteDoc } from 'firebase/firestore';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { db } from '../firebase.config';
+import ListingItem from '../components/ListingItem';
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
 import homeIcon from '../assets/svg/homeIcon.svg';
 
@@ -11,6 +12,8 @@ function Profile() {
   const auth = getAuth();
 
   const [changeDetails, setChangeDetails] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
@@ -19,6 +22,25 @@ function Profile() {
   const navigate = useNavigate();
 
   const { name, email } = formData;
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingRef = collection(db, 'listings');
+      const q = query(listingRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'));
+      const querySnap = await getDocs(q);
+
+      const listings = [];
+      querySnap.forEach(doc => {
+        listings.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    };
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   const onLogOut = () => {
     auth.signOut();
@@ -47,6 +69,15 @@ function Profile() {
       }
     } catch (error) {
       toast.error('Error updating profile');
+    }
+  };
+
+  const onDelete = async listingId => {
+    if (window.confirm('Are you sure, you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingId));
+      const updatedListings = listings.filter(listing => listing.id !== listingId);
+      setListings(updatedListings);
+      toast.success('Listings deleted successfully!');
     }
   };
 
@@ -96,6 +127,22 @@ function Profile() {
         <p>Sell or Rent your home</p>
         <img src={arrowRight} alt="arrowRight" />
       </Link>
+
+      {!loading && listings?.length > 0 && (
+        <>
+          <p className="listingText">Your Listings</p>
+          <ul className="listingsList">
+            {listings.map(listing => (
+              <ListingItem
+                key={listing.id}
+                listing={listing.data}
+                id={listing.id}
+                onDelete={() => onDelete(listing.id)}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
