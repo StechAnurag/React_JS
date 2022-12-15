9fe5da7743e3bbedc8a420cb2cd40386a2e27019
Anurag Shrivastava <anurag.dev443@gmail.com>
2022-12-15 20:52:22 +0530

    Home, login, register ui

--- mern-project/frontend/src/App.js
+++ mern-project/frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -17,6 +19,19 @@ function App() {
           </Routes>
         </div>
       </Router>
+
+      {/* The below toast options are default, we don't need to pass it. Though passing it for reference purpose */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
