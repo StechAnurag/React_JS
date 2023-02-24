eb2c92144b10dffc38729fb5af890a8cd1366be1
Anurag Shrivastava <anurag.dev443@gmail.com>
2023-02-24 21:24:04 +0530

    notes backend

--- mern-project/backend/routes/ticketRoutes.js
+++ mern-project/backend/routes/ticketRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { getTickets, createTicket, getTicket, updateTicket, deleteTicket } = require('../controllers/ticketController');
 const { protect } = require('../middlewares/authMiddleware');
+const noteRoutes = require('./noteRoutes');
+
+router.use('/:ticketId/notes', noteRoutes);
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 router.route('/:ticketId').get(protect, getTicket).put(protect, updateTicket).delete(protect, deleteTicket);
